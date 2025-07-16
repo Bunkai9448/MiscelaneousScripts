5f4ffc14f9826bd5109fc339943d1103fd384a04
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2025-07-16 11:39:32 +0200

    Codec reproducible en televisores OKI V19N-PHTUV

---
+++ OKI_Audio2TV.bat
@@ -0,0 +1,38 @@
+REM Para convertir audios a un formato reproducible en televisores OKI modelo V19N-PHTUV
+
+
+@echo off
+setlocal enabledelayedexpansion
+
+REM Define la carpeta de entrada aqui
+set "INPUT_FOLDER=2025-julio"
+
+REM Verifica si ffmpeg esta instalado
+where ffmpeg >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo FFmpeg no esta instalado o no se encuentra en el PATH.
+    echo Por favor, asegurate de que ffmpeg.exe este en la misma carpeta que este script o en el PATH del sistema.
+    pause
+    exit /b
+)
+
+REM Crea una carpeta de salida si no existe
+if not exist "AudioEnTV" mkdir AudioEnTV
+
+REM Verifica si la carpeta de entrada existe
+if not exist "%INPUT_FOLDER%" (
+    echo La carpeta '%INPUT_FOLDER%' no existe.
+    pause
+    exit /b
+)
+
+REM Procesa cada archivo MP3 en la carpeta de entrada
+for %%F in ("%INPUT_FOLDER%\*.mp3") do (
+    echo Procesando: %%F
+    ffmpeg -i "%%F" -codec:a libmp3lame -b:a 128k "AudioEnTV\tv_%%~nxF"
+)
+
+echo.
+echo Conversion completada. Los archivos se encuentran en la carpeta 'AudioEnTV'.
+
+pause
---
+++ OKI_Video2TV.bat
@@ -0,0 +1,38 @@
+REM Para convertir videos a un formato reproducible en televisores OKI modelo V19N-PHTUV
+
+
+@echo off
+setlocal enabledelayedexpansion
+
+REM Define la carpeta de entrada aqui
+set "INPUT_FOLDER=Marmalade"
+
+REM Verifica si ffmpeg esta instalado
+where ffmpeg >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo FFmpeg no esta instalado o no se encuentra en el PATH.
+    echo Por favor, asegurate de que ffmpeg.exe este en la misma carpeta que este script o en el PATH del sistema.
+    pause
+    exit /b
+)
+
+REM Crea una carpeta de salida si no existe
+if not exist "aVideo2TV" mkdir aVideo2TV
+
+REM Verifica si la carpeta de entrada existe
+if not exist "%INPUT_FOLDER%" (
+    echo La carpeta '%INPUT_FOLDER%' no existe.
+    pause
+    exit /b
+)
+
+REM Procesa cada archivo MP3 en la carpeta de entrada
+for %%F in ("%INPUT_FOLDER%\*.mkv" "%INPUT_FOLDER%\*.mp4") do (
+    echo Procesando: %%F
+    ffmpeg -i "%%F" -vf scale=720:480:flags=lanczos -codec:v libxvid -b:v 1000k -codec:a libmp3lame -b:a 12k -af aresample=async=1 -f avi "aVideo2TV\tv_%%~nF.avi"
+)
+
+echo.
+echo Conversion completada. Los archivos se encuentran en la carpeta 'aVideo2TV'.
+
+pause
